5e727992c503e871dd6fc707d9d3cdef9dc98563
zhengchaoqun <zhengchaoqun@ucredit.com>
2017-08-28 09:42:20 +0800

    控制网格

--- src/main/webapp/js/app/controller/UserController.js
+++ src/main/webapp/js/app/controller/UserController.js
@@ -1,19 +1,16 @@
 Ext.define('AM.controller.UserController', {
-    extend: 'Ext.app.Controller',
-
-    init: function() {
-      this.control({
-          'viewport > panel': {
-              render: this.onPanelRendered
-          }
-      });
-  },
-  views: [
-      'user.List'
-  ],
-
-  onPanelRendered: function() {
-      console.log('The panel was rendered');
-  }
-
-});
+	extend : 'Ext.app.Controller',
+	init : function() {
+		this.control({
+			'viewport > panel' : { render : this.onPanelRendered },
+			'userlist' : { itemdblclick : this.editUser } });
+	},
+	views : [
+		'user.List'
+	],
+	onPanelRendered : function() {
+		console.log('The panel was rendered');
+	},
+	editUser : function(grid, record) {
+		console.log('Double clicked on ' + record.get('name'));
+	} });
