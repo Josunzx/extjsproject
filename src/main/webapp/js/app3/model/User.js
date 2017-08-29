36370ab0e49170321b8c81e03a4658ef6c6aa503
zhengchaoqun <zhengchaoqun@ucredit.com>
2017-08-29 15:48:54 +0800

    EXTJS验证模块增加

--- src/main/webapp/js/app3/model/User.js
+++ src/main/webapp/js/app3/model/User.js
@@ -3,20 +3,24 @@ Ext.define('User', {
 	fields : [
 		'id', 'username'
 	],
-	//在model中使用proxy有如下好处：
-	//1.使每个使用User模型类的存储类实例以相同方式加载数据，这样能避免必须为每个存储类实例复制相同代理方式的定义。
-	//2.可以不必使用存储类来加载和保存模型数据
+	// 在model中使用proxy有如下好处：
+	// 1.使每个使用User模型类的存储类实例以相同方式加载数据，这样能避免必须为每个存储类实例复制相同代理方式的定义。
+	// 2.可以不必使用存储类来加载和保存模型数据
 	proxy : {
 		type : 'rest',
 		url : '/firstMaven/user/getUsers',
 		reader : { type : 'json', root : 'users' } },
-	hasMany : 'Post' // shorthand for { model: 'Post', name: 'posts' } 
-	}); 
-
-
-
-//加载User使用ID 1和相关的文章和评论使用User的代理 
-//将会调用data/users/1,到后端查找
+	validations : [//模型类验证数据的功能
+		{ type : 'presence', name : 'name' },
+		{ type : 'length', name : 'name', min : 5 },
+		{ type : 'format', name : 'age', matcher : /\d+/ },
+		{ type : 'inclusion', name : 'gender', list : [ 'male', 'female' ] },
+		{ type : 'exclusion', name : 'name', list : [ 'admin' ] }
+	],
+	hasMany : 'Post' // shorthand for { model: 'Post', name: 'posts' }
+});
+// 加载User使用ID 1和相关的文章和评论使用User的代理
+// 将会调用data/users/1,到后端查找
 User.load(1, { success : function(user) {
 	console.log("User: " + user.get('name'));
 	user.posts().each(function(post) {
@@ -25,4 +29,4 @@ User.load(1, { success : function(user) {
 			console.log(comment.get('message'));
 		});
 	});
-} }); 
+} });
