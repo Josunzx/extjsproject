9e368e79b3e28e8b9f7787b17cf4ad61beff8137
zhengchaoqun <zhengchaoqun@ucredit.com>
2017-08-29 11:27:30 +0800

    No Session found for current thread bug修改，修改方法： 事务由配置改为注解，并在配置中添加启动注解的代码：<tx:annotation-driven transaction-manager="transactionManager"/>

--- src/main/java/com/book/service/impl/BookServiceImpl.java
+++ src/main/java/com/book/service/impl/BookServiceImpl.java
@@ -2,12 +2,14 @@ package com.book.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.book.dao.BookDao;
 import com.book.entity.Book;
 import com.book.service.BookService;
 
 @Service
+@Transactional(rollbackFor = Exception.class)
 public class BookServiceImpl implements BookService {
     @Autowired
     private BookDao bookDao;
