4cb08623862245fcb8a71b92f762fb2022048f09
zhengchaoqun <zhengchaoqun@ucredit.com>
2017-08-29 19:21:46 +0800

    数据库导出

---
+++ src/main/webapp/sql/book.sql
@@ -0,0 +1,2 @@
+INSERT INTO `book` (`ID`, `BOOK_NAME`, `ISBN`, `PRICE`, `STOCK`) VALUES (1, 'JVM深入理解', '走走', 111, 11);
+INSERT INTO `book` (`ID`, `BOOK_NAME`, `ISBN`, `PRICE`, `STOCK`) VALUES (2, 'android源码分析', '1002', 45, 10);
---
+++ src/main/webapp/sql/user.sql
@@ -0,0 +1,1 @@
+INSERT INTO `user` (`ID`, `USERNAME`, `PASSWORD`, `SEX`, `AGE`, `XUEHAO`, `BANJI`, `BIRTH`) VALUES (1, 'admin', 'admin', 1, 0, NULL, NULL, NULL);
